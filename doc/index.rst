1197ed558e66d7bc8eeb9006aed8d239716897c7
Tatiana Quizhpe <tatianaq@nodux.ec>
2016-04-05 15:49:22 -0500

    First release

---
+++ doc/index.rst
@@ -0,0 +1,4 @@
+Nodux Account Statement
+#######################################################
+
+Modulo para poner en 0 el balance inicial del estado de cuenta (utilizado para venta TPV).
---
+++ statement.py
@@ -0,0 +1,45 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+from decimal import Decimal
+from collections import namedtuple
+from itertools import groupby
+
+from sql.aggregate import Sum
+
+from trytond.model import Workflow, ModelView, ModelSQL, fields
+from trytond.pyson import Eval, If, Bool
+from trytond.transaction import Transaction
+from trytond import backend
+from trytond.pool import Pool, PoolMeta
+from trytond.modules.company import CompanyReport
+from trytond.tools import reduce_ids
+
+__all__ = ['Statement']
+__metaclass__ = PoolMeta
+
+class Statement():
+    'Account Statement'
+    __name__ = 'account.statement'
+
+    @classmethod
+    def __setup__(cls):
+        super(Statement, cls).__setup__()
+
+    @fields.depends('journal', 'state', 'lines')
+    def on_change_journal(self):
+        res = {}
+        if not self.journal:
+            return res
+
+        statements = self.search([
+                ('journal', '=', self.journal.id),
+                ], order=[
+                ('date', 'DESC'),
+                ], limit=1)
+        if not statements:
+            return res
+
+        statement, = statements
+        res['start_balance'] = Decimal(0.0)
+        return res
+
